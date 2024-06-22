2ec58252698a722159e61fb20e61eb128ae1d738
umorih~ <umorih.net@gmail.com>
2024-06-22 15:18:18 +0900

    設定ファイルの追加

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -2,6 +2,22 @@ import nextTranslate from 'next-translate-plugin';
 
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: "https",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.jp",
+      },
+    ],
+  },
 };
 
 export default nextTranslate(nextConfig); // Next.jsの設定オブジェクトに、多言語対応の設定を組み込む役割
