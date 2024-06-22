980001277511e03e18035ff3716828e8b1a98d44
umorih~ <umorih.net@gmail.com>
2024-06-22 14:04:06 +0900

    next-translate で言語ファイルを追加

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import useTranslation from 'next-translate/useTranslation';
 
 export default function Home() {
+  const { t } = useTranslation('common');
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <Button>クリック！</Button>
+        <Button>{t('app.button.title')}</Button>
       </div>
     </main>
   );
--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextTranslate from 'next-translate-plugin';
 
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default nextTranslate(nextConfig); // Next.jsの設定オブジェクトに、多言語対応の設定を組み込む役割
