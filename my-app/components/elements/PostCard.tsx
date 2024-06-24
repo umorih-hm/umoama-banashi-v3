62f5d98f7bef1f26962ca7a93e6c2083b21f2653
umorih~ <umorih.net@gmail.com>
2024-06-24 23:22:21 +0900

    postcard に tooltip を入れてみた

--- my-app/components/elements/PostCard.tsx
+++ my-app/components/elements/PostCard.tsx
@@ -1,36 +1,50 @@
 import Link from 'next/link';
 import NextImage from 'next/image';
-import { User } from '@nextui-org/react';
+import { User, Tooltip } from '@nextui-org/react';
+
+import useTranslation from 'next-translate/useTranslation';
 
 export const PostCard = ({ post, index }: PostCardRequest) => {
+  const { t } = useTranslation('common');
+
   return (
-    <Link href={`/blog/${post.id}`} key={index}>
-      {/* サムネ画像 */}
-      <NextImage
-        width="200"
-        height="100"
-        alt={post.title}
-        className="object-cover h-2/5 rounded-lg hover:opacity-50"
-        src={post.image ? post.image : 'https://placehold.jp/200x150.png'}
-      />
-      {/* タイトル */}
-      <h2 className="font-bold my-2">{post.title}</h2>
+    <Tooltip
+      content={
+        <div className="px-1 py-2">
+          <div className="text-small text-white font-bold">{t('component.post_card.sub_title')}</div>
+          <div className="text-tiny text-white">{post.subTitle}</div>
+        </div>
+      }
+      isDisabled={!post.subTitle}
+      color="success"
+    >
       {/* サブブタイトル */}
-      <h6 className="mb-2">{post.subTitle}</h6>
-      {/* パーソン ・日付*/}
-      <User
-        name={post.person}
-        avatarProps={
-          post.person === 'UMORiH'
-            ? {
-                src: '/Umorih.png',
-              }
-            : {
-                src: '/Amaneriy.png',
-              }
-        }
-        description={`投稿日：${post.date}`}
-      />
-    </Link>
+      <Link href={`/blog/${post.id}`} key={index}>
+        {/* サムネ画像 */}
+        <NextImage
+          width="200"
+          height="100"
+          alt={post.title}
+          className="object-cover h-2/5 rounded-lg hover:opacity-50"
+          src={post.image ? post.image : 'https://placehold.jp/200x150.png'}
+        />
+        {/* タイトル */}
+        <h2 className="font-bold my-2">{post.title}</h2>
+        {/* パーソン ・日付*/}
+        <User
+          name={post.person}
+          avatarProps={
+            post.person === 'UMORiH'
+              ? {
+                  src: '/Umorih.png',
+                }
+              : {
+                  src: '/Amaneriy.png',
+                }
+          }
+          description={`投稿日：${post.date}`}
+        />
+      </Link>
+    </Tooltip>
   );
 };
